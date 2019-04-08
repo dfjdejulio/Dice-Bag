d384d01a3e3bb38fa58c7ea34af3de1efedd50a4
Doug DeJulio <ddj@aisb.org>
2019-04-08 19:07:43 -0400

    First draft of storyboard, placeholders for icons, and copy/paste of some die-rolling code I may have first written in the 1980s.

---
+++ Engine/roll.h
@@ -0,0 +1,16 @@
+//
+//  roll.h
+//  Dice Bag
+//
+//  Created by Doug DeJulio on 2019-04-08.
+//  Copyright © 2019 Doug DeJulio. All rights reserved.
+//
+
+#ifndef roll_h
+#define roll_h
+
+int roll(int best, int dice, int die);
+
+//int rolls(char *dstring);
+
+#endif /* roll_h */
